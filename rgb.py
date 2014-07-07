6803df9179a5abbe0ca7e6f41a645cfbee322466
Nataly López <natalylopez380@hotmail.com>
2014-07-07 12:30:23 -0430

    Update rgb.py

--- rgb.py
+++ rgb.py
@@ -5,7 +5,7 @@
   [RGB] Bot!
 
   Copyright (C) 2013 Crozz Cyborg <CrozzCyborg@hotmail.es> 
-                2014 Nataly Lopez <natalylopez380@hotmail.com>
+  Copyright (C) 2014 Nataly Lopez <natalylopez380@hotmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
