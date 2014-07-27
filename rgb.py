0d32093831723c487e45989068bc4aa377f9d42f
Nataly López <natalylopez380@hotmail.com>
2014-07-26 21:01:40 -0430

    Update rgb.py

--- rgb.py
+++ rgb.py
@@ -4,8 +4,8 @@
 """
   [RGB] Bot!
 
+  Copyright (C) 2014 Nataly Lopez <natalylopez380@hotmail.com>
   Copyright (C) 2013 Crozz Cyborg <CrozzCyborg@hotmail.es> 
-                2014 Nataly Lopez <natalylopez380@hotmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see http://www.gnu.org/licenses/.
+
 """
 
 import socket,re,sys
@@ -55,13 +56,37 @@ def JoinPart(Canal,accion,irc): # Funcion para salir o entrar de un canal Sintax
 		#irc.send("PRIVMSG "+Canal+" :Welcome :D\n")
 
 def EncenderLed(Canal,irc):
-	 bridge.put('D13','1') #esto enciende led 
-	 irc.send("PRIVMSG "+Canal+" :led encendido\n")
-
+         bridge.put('D13','1') #esto enciende led de status
+         irc.send("PRIVMSG "+Canal+" :led encendido\n")
+                  
 def ApagarLed(Canal,irc):
-         bridge.put('D13','0') #esto apaga led
+         bridge.put('D13','0') #esto apaga led de status
          irc.send("PRIVMSG "+Canal+" :led apagado\n")
-		 
+                                    
+def EncenderRojo(Canal,irc):
+	 bridge.put('D02','1') #esto enciende led rojo
+	 irc.send("PRIVMSG "+Canal+" :led rojo encendido\n")
+
+def ApagarRojo(Canal,irc):
+         bridge.put('D02','0') #esto apaga led rojo
+         irc.send("PRIVMSG "+Canal+" :led rojo apagado\n")
+                  
+def EncenderVerde(Canal,irc):
+         bridge.put('D03','1') #esto enciende led verde
+         irc.send("PRIVMSG "+Canal+" :led verde encendido\n")
+         
+def ApagarVerde(Canal,irc):
+         bridge.put('D03','0') #esto apaga led verde
+         irc.send("PRIVMSG "+Canal+" :led verde apagado\n")
+                  
+def EncenderAzul(Canal,irc):
+         bridge.put('D04','1') #esto enciende led azul
+         irc.send("PRIVMSG "+Canal+" :led azul encendido\n")
+
+def ApagarAzul(Canal,irc):
+         bridge.put('D04','0') #esto apaga led azul
+         irc.send("PRIVMSG "+Canal+" :led azul apagado\n")
+                                    		 
 def MDatos(data,irc): # Manipulacion de datos
 	if re.match(r'^PING :',data): # Se envia pong
 		irc.send("PONG :"+data[6:]+"\n")
@@ -73,11 +98,22 @@ def MDatos(data,irc): # Manipulacion de datos
 		tmp = data.split(':')
 		#print len(tmp)
 		if len(tmp) > 2:
-			if re.match(r'on\!', tmp[2]): # Si se recibe el on!
+			if re.match(r'on\!', tmp[2]):   # Si se recibe on!
 				EncenderLed(canal, irc)
-			elif re.match(r'off\!',tmp[2]): # Si se recibe el off!
+			elif re.match(r'off\!',tmp[2]): # Si se recibe off!
 				ApagarLed(canal, irc)
-
+			elif re.match(r'rojo on\!',tmp[2]): # Si se recibe rojo on!
+				EncenderRojo(canal, irc)
+                        elif re.match(r'rojo off\!',tmp[2]): # Si se recibe rojo off!
+                                ApagarRojo(canal, irc)
+                        elif re.match(r'verde on\!',tmp[2]): # Si se recibe verde on!
+                                EncenderVerde(canal, irc)
+                        elif re.match(r'verde off\!',tmp[2]): # Si se recibe verde off!
+                                ApagarVerde(canal, irc)
+                        elif re.match(r'azul on\!',tmp[2]): # Si se recibe azul on!
+                                EncenderAzul(canal, irc)
+                        elif re.match(r'azul off\!',tmp[2]): # Si se recibe azul off!
+                                ApagarAzul(canal, irc)                                                 
 
 def main():
 	print "[!] RGBOT :) [!]\n"
